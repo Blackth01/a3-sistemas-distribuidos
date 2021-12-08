e660be018484db1a880457781e016e4e4187c5a2
LeonardsonCC <leonardson.carvalho@gmail.com>
2021-12-08 20:10:51 -0300

    feat: cors enabled

--- backend/go.mod
+++ backend/go.mod
@@ -4,7 +4,7 @@ go 1.16
 
 require (
 	github.com/go-sql-driver/mysql v1.6.0
-	github.com/labstack/echo/v4 v4.6.1 // indirect
+	github.com/labstack/echo/v4 v4.6.1
 	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	golang.org/x/crypto v0.0.0-20211117183948-ae814b36b871 // indirect
--- backend/main.go
+++ backend/main.go
@@ -1,34 +1,39 @@
 package main
 
 import (
-	"github.com/labstack/echo/v4"
-	"github.com/Blackth01/a3-sistemas-distribuidos/raw_material"
-	"github.com/Blackth01/a3-sistemas-distribuidos/product"
 	"github.com/Blackth01/a3-sistemas-distribuidos/input"
+	"github.com/Blackth01/a3-sistemas-distribuidos/product"
+	"github.com/Blackth01/a3-sistemas-distribuidos/raw_material"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
 
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	}))
+
 	// Raw material routes
 	e.POST("/raw_material", raw_material.CreateRawMaterial)
 	e.GET("/raw_material", raw_material.GetRawMaterials)
-    e.GET("/raw_material/:id", raw_material.GetRawMaterial)
-    e.GET("/raw_material/by_product/:id", raw_material.GetRawMaterialsByProduct)
-    e.PUT("/raw_material/:id", raw_material.PutRawMaterial)
-    e.DELETE("/raw_material/:id", raw_material.RemoveRawMaterial)
+	e.GET("/raw_material/:id", raw_material.GetRawMaterial)
+	e.GET("/raw_material/by_product/:id", raw_material.GetRawMaterialsByProduct)
+	e.PUT("/raw_material/:id", raw_material.PutRawMaterial)
+	e.DELETE("/raw_material/:id", raw_material.RemoveRawMaterial)
 
-    // Product routes
+	// Product routes
 	e.POST("/product", product.CreateProduct)
 	e.GET("/product", product.GetProducts)
-    e.GET("/product/:id", product.GetProduct)
-    e.PUT("/product/:id", product.PutProduct)
-    e.DELETE("/product/:id", product.RemoveProduct)
+	e.GET("/product/:id", product.GetProduct)
+	e.PUT("/product/:id", product.PutProduct)
+	e.DELETE("/product/:id", product.RemoveProduct)
 
-    // Input relation routes
+	// Input relation routes
 	e.POST("/input", input.CreateInput)
-    e.PUT("/input", input.PutInput)
-    e.DELETE("/input", input.RemoveInput)
+	e.PUT("/input", input.PutInput)
+	e.DELETE("/input", input.RemoveInput)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":9000"))
